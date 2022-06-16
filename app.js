32683ff2287db98318fcd0f3266a967677965d0d
holabisii <ajoseholabisi@gmail.com>
2022-06-16 11:31:02 -0400

    cors

--- app.js
+++ app.js
@@ -5,8 +5,34 @@ const __dirname = dirname(__filename)
 
 
 import express from 'express'
+import cors from 'cors'
 
 const app = express();
+
+
+// CORS PROBLEM ALLOWING FRONTEND TO PROCESS HTTP METHODS
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization"
+  );
+  next();
+});
+
+// CORS PROBLEM END
 
 const port = 7000
 
