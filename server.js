3f301fe61506b49542cdbec2089a04aea2daf3c1
holabisii <ajoseholabisi@gmail.com>
2022-06-16 14:09:35 -0400

    mongodb connection setup

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 import dotenv from 'dotenv'
 import app from './app.js'
+import mongoose from 'mongoose'
 
 
 dotenv.config({
         path: './.env'
 })
 
-
 const port = 7000
+
+const MONGO_URI = process.env.MONGO_URI
+
+mongoose.connect(MONGO_URI)
+        .then(connection => console.log("mongodb connected"))
+        .catch(err => console.log("error created"))
 
 app.get('/', (req, res) => {
         console.log("home page")
